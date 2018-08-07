f2d0897f4ccc26b46226064ffa23c84f871e7ff3
Jeffrey Webb <jeff@MacBook-Pro.localdomain>
2018-08-07 19:03:01 -0400

    connected to api

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Cloudy
+
+
+Requirements
+Using openweathermap.org, create a simple UI that allows a user to search for the current weather by zip code or city
+
+-temnp not in US format
+        -add '&unit=imperial'
+-add appID to url
+    http://api.openweathermap.org/data/2.5/weather?q=33635&appid=d2816c5d1df2915df983bc9434b35661
+
+    Base URL: const _url = `${BASE_URL}/tampa?search=${needle}`
+
+    const weatherKey = () => {
+       const key =  'd2816c5d1df2915df983bc9434b35661'
+        const WEATHER_BASE_URL = http://api.openweathermap.org/data/2.5/
+        const _zip = ' '
+        const _url = `${WEATHER_BASE_URL}weather?appid=${key}&{_zip}`
+      }
--- public/main.js
+++ public/main.js
@@ -2,4 +2,34 @@ const main = () => {
   document.querySelector('h1').textContent += '?'
 }
 
+const WEATHER_BASE_URL = 'http://api.openweathermap.org/data/2.5/'
+
+const searchAPI = () => {
+  const userInput = document.querySelector('.input').value
+
+  const key = 'd2816c5d1df2915df983bc9434b35661'
+  const WEATHER_BASE_URL = 'http://api.openweathermap.org/data/2.5/'
+
+  const _url = `${WEATHER_BASE_URL}weather?appid=${key}&q=${userInput}`
+
+
+  fetch(_url)
+    .then(resp => {
+      console.log("response = ", resp)
+      if (resp.status === 200) {
+        return resp.json()
+      } else {
+        consol.log("error", resp)
+      }}
+    ).then(searchResults => {
+      console.log("search results = ", searchResults)
+      const parent = document.querySelector()
+    })
+
+  console.log(userInput)
+}
+
+
 document.addEventListener('DOMContentLoaded', main)
+document.querySelector('button').addEventListener('click', searchAPI)
+
